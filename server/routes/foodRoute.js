8dfe4629e4a3fa21c21fd2f12309dbdde779210a
EslamElnajdy <eslammhamed425@gmail.com>
2024-09-24 02:54:15 +0300

    chore: Update npm dependencies for server package

--- server/routes/foodRoute.js
+++ server/routes/foodRoute.js
@@ -1,17 +1,27 @@
 import express from "express";
 import { addFood, listFood, removeFood } from "../controllers/foodController.js";
 import multer from "multer";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 const foodRouter = express.Router();
 
-// Image Storage Engine
+// Configure Cloudinary
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
-const storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`)
-  }
-})
+// Set up Cloudinary storage
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "uploads", // Folder in your Cloudinary storage
+    format: async (req, file) => "jpg", // Specify the image format
+    public_id: (req, file) => Date.now() + "_" + file.originalname, // Unique file name
+  },
+});
 
 const upload = multer({ storage: storage });
 
