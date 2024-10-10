4416890f350d018c5393a1b0f12dbb16133afd7d
EslamElnajdy <eslammhamed425@gmail.com>
2024-10-10 16:51:35 +0300

    chore: Update server deployment URL in StoreContext

--- client/src/context/StoreContext.jsx
+++ client/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
 
   const [cartItems, setCartItems] = useState({});
-  const url = "https://server-e4kt64gsf-eslamelnajdys-projects.vercel.app";
+  const url = "https://server-cysxaoj2q-eslamelnajdys-projects.vercel.app";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
